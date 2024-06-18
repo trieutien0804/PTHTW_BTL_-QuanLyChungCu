d7585c968d6e027cc9764f470348ae2a8d033f96
dungpham1903 <ptd.zuxg193@gmail.com>
2024-06-18 20:11:41 +0700

    aaa

--- target/generated-sources/annotations/com/chungcu/pojo/Bill_.java
+++ target/generated-sources/annotations/com/chungcu/pojo/Bill_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-06-18T19:39:18")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-06-18T20:08:25")
 @StaticMetamodel(Bill.class)
 public class Bill_ { 
 
