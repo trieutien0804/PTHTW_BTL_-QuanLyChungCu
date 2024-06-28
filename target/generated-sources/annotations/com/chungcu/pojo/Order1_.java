3078ac185ee5686a93dfecf71e8608bf4983a75c
dungpham1903 <ptd.zuxg193@gmail.com>
2024-06-28 14:51:22 +0700

    push

--- target/generated-sources/annotations/com/chungcu/pojo/Order1_.java
+++ target/generated-sources/annotations/com/chungcu/pojo/Order1_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-06-28T14:42:06")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-06-28T14:46:39")
 @StaticMetamodel(Order1.class)
 public class Order1_ { 
 
